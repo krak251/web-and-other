b596562b27f901b6d22b6e56afb334bc7e023b35
krak251 <botkinulsa@gmail.com>
2026-02-16 17:59:43 +0300

    колонизация марса

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "<h1>Миссия Колонизация Марса</h1>"
+
+@app.route("/index")
+def index():
+    return "<h1>И на Марсе будут яблони цвести!</h1>"
+
+if __name__ == "__main__":
+    app.run("127.0.0.1", 8080)
