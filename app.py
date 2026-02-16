ebabc9dd4033c700590955b374cb8c1afa492a3c
krak251 <botkinulsa@gmail.com>
2026-02-16 18:03:24 +0300

    Рекламная кампания

--- app.py
+++ app.py
@@ -2,13 +2,27 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def main():
     return "<h1>Миссия Колонизация Марса</h1>"
 
+
 @app.route("/index")
 def index():
     return "<h1>И на Марсе будут яблони цвести!</h1>"
+
+
+@app.route("/promotion")
+def promotion():
+    return """
+    <h1>Человечество вырастает из детства.</h1>
+    <h2>Человечеству мала одна планета.</h2>
+    <h3>Мы сделаем обитаемыми безжизненные пока планеты.</h3>
+    <h4>И начнем с Марса!</h4>
+    <h1>Присоединяйся!</h1>
+    
+    """
 
 if __name__ == "__main__":
     app.run("127.0.0.1", 8080)
