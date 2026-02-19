3a7d2a538bbd766f7c63bd5c29f3740e2614ae6e
krak251 <botkinulsa@gmail.com>
2026-02-19 18:15:17 +0300

    Отбор астронавтов

--- app.py
+++ app.py
@@ -109,5 +109,10 @@ def results(nickname, level, rating):
     return render_template("results.html", nickname=nickname, level=level, rating=rating)
 
 
+@app.route("/astronaut_selection")
+def astronaut_selection():
+    return render_template("astronaut_selection.html")
+
+
 if __name__ == "__main__":
     app.run("127.0.0.1", 8080)
