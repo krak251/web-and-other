86332ba2b5d1eb8e9982d9bf071196e35be3f4a7
krak251 <botkinulsa@gmail.com>
2026-03-19 19:00:59 +0300

    до обработчика формы авторизации

--- app.py
+++ app.py
@@ -3,17 +3,23 @@ from data import db_session
 from data.jobs import Jobs
 from data.users import User
 from forms.Login_form import LoginForm
+from flask_login import LoginManager, login_user, login_required, logout_user
 import flask_wtf
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "password1921"
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def user_loader(user_id):
+    session = db_session.create_session()
+    return session.get(User, user_id)
+
+
 @app.route("/")
-def main():
-    return render_template("main.html")
-
-
 @app.route("/index")
 def index():
     session = db_session.create_session()
@@ -159,8 +165,25 @@ def answer():
 def login():
     login_form = LoginForm()
     if login_form.validate_on_submit():
-        return redirect("/")
+        session = db_session.create_session()
+        user = session.query(User).filter(
+            User.email == login_form.email.data
+        ).first()
+        if user and user.check_password(login_form.password.data):
+            login_user(user, login_form.remember_me.data)
+            return redirect("/")
+        else:
+            return render_template("login.html", form=login_form,
+                                   message="Пользователь не существует.")
     return render_template("login.html", form=login_form)
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
 
 @app.route("/load_photo", methods=['POST', 'GET'])
 def load_photo():
